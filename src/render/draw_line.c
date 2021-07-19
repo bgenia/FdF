b51e2e449506f7a501035be243ac2989c8e4595a
bgenia <bgenia@student.21-school.ru>
2021-07-19 20:33:54 +0300

    Fix draw_line

--- src/render/draw_line.c
+++ src/render/draw_line.c
@@ -9,6 +9,13 @@ static int	abs(int n)
 	return (n);
 }
 
+static double	direction(int a, int b)
+{
+	if (a > b)
+		return (-1.);
+	return (1.);
+}
+
 void	draw_line(t_renderer *renderer, t_brush brush, t_spoint a, t_spoint b)
 {
 	double	tg;
@@ -18,14 +25,22 @@ void	draw_line(t_renderer *renderer, t_brush brush, t_spoint a, t_spoint b)
 	tg = (double)abs(a.y - b.y) / (double)abs(a.x - b.x);
 	x = a.x;
 	y = a.y;
-	while (x <= b.x && y <= b.y)
+	while ((int)floor(x) != b.x || (int)floor(y) != b.y)
 	{
 		draw_pixel(
 			renderer,
 			brush,
 			(t_spoint){.x = (int)floor(x), .y = (int)floor(y)}
 			);
-		x += 1;
-		y += tg;
+		if (tg < 1)
+		{
+			x += 1 * direction(a.x, b.x);
+			y += tg * direction(a.y, b.y);
+		}
+		else
+		{
+			x += 1 / tg * direction(a.x, b.x);
+			y += 1 * direction(a.y, b.y);
+		}
 	}
 }
