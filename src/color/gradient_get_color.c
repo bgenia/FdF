b3f4b3f3a548bc085d5da604e5cccea95182d2d8
bgenia <bgenia@student.21-school.ru>
2021-07-27 13:28:45 +0300

    Fix gradient_get_color

--- src/color/gradient_get_color.c
+++ src/color/gradient_get_color.c
@@ -11,13 +11,13 @@ t_color	gradient_get_color(t_color from, t_color to, int step, int n)
 	double	g;
 	double	b;
 
-	a = color_get_alpha(from) + ((double)n / step)
+	a = color_get_alpha(from) + ((double)n / (step + 1))
 		* (color_get_alpha(to) - color_get_alpha(from));
-	r = color_get_red(from) + ((double)n / step)
+	r = color_get_red(from) + ((double)n / (step + 1))
 		* (color_get_red(to) - color_get_red(from));
-	g = color_get_green(from) + ((double)n / step)
+	g = color_get_green(from) + ((double)n / (step + 1))
 		* (color_get_green(to) - color_get_green(from));
-	b = color_get_blue(from) + ((double)n / step)
+	b = color_get_blue(from) + ((double)n / (step + 1))
 		* (color_get_blue(to) - color_get_blue(from));
 	return (color_argb(round(a), round(r), round(g), round(b)));
 }
