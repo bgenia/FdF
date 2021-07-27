708497366279b21075c089c26c8cdfb2f6b4120e
bgenia <bgenia@student.21-school.ru>
2021-07-27 13:48:20 +0300

    FIX VERTICAL ALIGNMENT FINALLY

--- src/scene/view_create.c
+++ src/scene/view_create.c
@@ -1,6 +1,18 @@
+#include "libft/math.h"
+
 #include "scene.h"
 #include "projection.h"
 #include "heightmap.h"
+
+static double	compute_z_offset(t_heightmap map)
+{
+	double	offset;
+
+	offset = 0;
+	offset -= map.min_elevation;
+	offset -= (double)(map.max_elevation - map.min_elevation) / 2;
+	return (offset);
+}
 
 t_view	view_create(t_heightmap map, t_projection projection)
 {
@@ -11,7 +23,7 @@ t_view	view_create(t_heightmap map, t_projection projection)
 	view.center_offset = double3(
 			-(double)map.width / 2,
 			-(double)map.height / 2,
-			(-(double)(map.max_elevation - map.min_elevation)) / 2
+			compute_z_offset(map)
 			);
 	view.render_offset = double2(0, 0);
 	view.projection = projection;
